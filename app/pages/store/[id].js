ef1f2a05efb991ff5930ad01c97c70e6a15ab971
Maria Peregrina <mperegrina@edu.tecnocampus.cat>
2021-04-17 19:20:41 +0200

    Restaurant

--- app/pages/store/[id].js
+++ app/pages/store/[id].js
@@ -1,1 +1,18 @@
-export default function store() {}
+import Head from "next/head";
+import Header from "../../components/header";
+import styles from "../../styles/Home.module.css";
+
+export default function store() {
+    return (
+     <>
+    <Header showTransparent={false} />
+    <main>
+        <div className={styles.imagen}></div>
+        <div>
+            <h1 className={styles.nombre} >Burguer Mar</h1>
+        </div>
+
+    </main>
+     </>
+    )
+}

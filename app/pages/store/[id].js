d705f0bee9d6f1e80f2a1d12cea667a8b875b4ad
Erik Pastor <erik.pastor@glovoapp.com>
2021-04-17 19:15:58 +0200

    Fix store

--- app/pages/store/[id].js
+++ app/pages/store/[id].js
@@ -1,12 +1,8 @@
 import { useRouter } from "next/router";
 
 export default function store() {
-  const Post = () => {
-    const router = useRouter();
-    const { pid } = router.query;
+  const router = useRouter();
+  const { pid } = router.query;
 
-    return <p>Post: {pid}</p>;
-  };
-
-  export default Post;
+  return <p>Post: {pid}</p>;
 }

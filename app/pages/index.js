dccbf927c9b659d127eaedd38660dd6a31d508c7
Erik Pastor <erik.pastor@glovoapp.com>
2021-04-18 10:39:57 +0200

    Update a la landing page

--- app/pages/index.js
+++ app/pages/index.js
@@ -43,8 +43,58 @@ export default function Home() {
       <Header showTransparent={showTransparent} />
       <main>
         <div className={styles.landing} id="landing">
-            <h2 className={styles.slogan}>COME A MEJOR PRECIO EN TU RESTAURANTE HABITUAL</h2>
-          <h3 className={styles.slogan}> Consigue descuentos exlusivos pagando por adelantado tus menús</h3>
+          <div>
+            <h2 className={styles.slogan}>
+              COME A MEJOR PRECIO EN TU RESTAURANTE HABITUAL
+            </h2>
+            <h3 className={styles.slogan}>
+              Consigue descuentos exlusivos pagando por adelantado tus menús
+            </h3>
+          </div>
+          <button>Subscribete a tu restaurante local favorito</button>
+        </div>
+
+        <div className={styles.foodWaste}>
+          <div className={styles.foodWaste__text}>
+            <h2>
+              AYUDANOS A INCREMENTAR EL CONSUMO RESPONSABLE Y DE PROXIMIDAD
+            </h2>
+            <h3>EL PLANETA Y SUS HABITANTES TE LO AGRADECERAN!</h3>
+            <p>
+              El consumo de proximidad es respetuoso con el medio ambiente, es
+              económico, disminuye la denominada “huella de carbono” y respeta
+              la continuidad de la biodiversidad de nuestros ecosistemas.
+              Reducir la pérdida y el desperdicio de alimentos puede incrementar
+              la disponibilidad de comida y, por lo tanto, hacer que esta sea
+              más asequible para los consumidores sin incrementar la producción
+              ni continuar desgastando los recursos naturales.
+            </p>
+          </div>
+          <div className={styles.foodWaste__cifras}>
+            <div className={`${styles.foodWaste__card}`}>
+              <h3>10%</h3>
+              <p>reducción media en tu ticket de menú</p>
+            </div>
+            <div className={`${styles.foodWaste__card}`}>
+              <h3>1</h3>
+              <p>millón de kg de comida no malgastada des de 2021</p>
+            </div>
+            <div className={`${styles.foodWaste__card}`}>
+              <h3>34</h3>
+              <p>
+                restaurantes que ya se han unido al consumo local y responsable
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.banner}>
+          <div className={styles.banner__text}>
+            <h3>
+              Únete al consumo local y colabora ambiental, social y
+              economicamente
+            </h3>
+          </div>
         </div>
 
         <div className={styles.topFiltros}>
@@ -58,9 +108,9 @@ export default function Home() {
             ))}
           </div>
         </div>
-        <div className={styles.banner}>
+        <div className={`${styles.banner} ${styles.banner__bussiness}`}>
           <div className={styles.banner__text}>
-            <h3>El consumo local és un factor muy importante para conseguir beneficios ambientales, sociales y económicos</h3>
+            <h3>La revolución 4.0 para tu negocio ya está aquí!</h3>
           </div>
         </div>
         <div className={styles.bussiness}>
@@ -79,12 +129,6 @@ export default function Home() {
                 </div>
               </a>
             ))}
-          </div>
-        </div>
-        <div className={`${styles.banner} ${styles.banner__bussiness}`}>
-          <div className={styles.banner__text}>
-            <h3>Frase para los negocios</h3>
-            <p>Frase más larga para los negocios</p>
           </div>
         </div>
         <footer className={styles.footer}>
